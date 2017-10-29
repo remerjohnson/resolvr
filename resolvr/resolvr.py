7be0ce790ce78b824ab46a2684d66f45dfba72d0
remerjohnson <ryan.emerson.johnson@gmail.com>
2017-10-28 18:52:51 -0700

    Got app to take file input and display dataframe as HTML table. TODO: get CSS to style table. Begin work on form that queries by ID, then finally get the file input form to query Wikidata

--- resolvr/resolvr.py
+++ resolvr/resolvr.py
@@ -8,6 +8,7 @@ import os
 from flask import *
 import pandas as pd
 from werkzeug.utils import secure_filename
+
 # Declare and load the app
 app = Flask(__name__)
 
@@ -21,9 +22,27 @@ def allowed_file(filename):
 def home(name=None):
     return render_template('home.html', name=name)
 
-@app.route('/query', methods=['GET', 'POST'])
-def query(name=None):
-    return render_template('query.html', name=name)
+@app.route('/single_query')
+def single_query(name=None):
+    return render_template('single_query.html', name=name)
+
+@app.route('/lookup_view', methods=['POST'])
+def lookup_view():
+    return render_template('single_query.html', name=name)
+
+@app.route('/batch_query')
+def batch_query(name=None):
+    return render_template('batch_query.html', name=name)
+
+@app.route('/data_view', methods=['POST'])
+def data_view():
+    file = request.files['data_file']
+    if not file:
+        return "No file"
+
+    df = pd.read_csv(file)
+
+    return df.to_html(classes='table table-striped')
 
 
 if __name__ == '__main__':
